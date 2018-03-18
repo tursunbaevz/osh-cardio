34bf5ba8c59d9c3f243effe5b28edf17cb9a54de
tursunbaevz <zuma9405@gmail.com>
2018-03-18 22:40:22 +0600

    make responsive slider

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -36,24 +36,24 @@
 				                <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
 				            </ol>
 				            <div class="carousel-inner" role="listbox">
-				            <!--     <div class="carousel-item">
+				                <div class="carousel-item">
 				                    <img class="d-block" src="/hospital.jpg" alt="First slide">
 				                    <div class="carousel-caption d-none d-md-block">
 				                        <h5>Nature, United States</h5>
 				                    </div>
-				                </div> -->
+				                </div>
 				                <div class="carousel-item active">
 				                    <img class="d-block" src="/hospital.jpg" alt="Second slide">
 				                    <div class="carousel-caption d-none d-md-block">
 				                        <h5>Somewhere Beyond, United States</h5>
 				                    </div>
 				                </div>
-				                <!-- <div class="carousel-item">
+				                <div class="carousel-item">
 				                    <img class="d-block" src="/hospital.jpg" alt="Third slide">
 				                    <div class="carousel-caption d-none d-md-block">
 				                        <h5>Yellowstone National Park, United States</h5>
 				                    </div>
-				                </div> -->
+				                </div>
 				            </div>
 				            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 				                <i class="now-ui-icons arrows-1_minimal-left"></i>
