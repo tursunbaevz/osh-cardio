905aa3a7e9f1ec2bbc0d672f94a5c731a1520779
Kozhonazar <rysbaev.k@gmail.com>
2018-03-18 21:05:46 +0600

    Slider added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,9 +24,54 @@
 <!-- наш слайд -->
 <div class="wrapper">
 	<div class="page-header page-header-small">
+		<div class="page-header">
 
 
-		    <div class="page-header-image" data-parallax="true" style="background-image: url('/hospital.jpg');">
+
+<!-- слайдер -->
+
+	<div class="row justify-content-center" style="height: 100%">
+
+	        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	            <ol class="carousel-indicators">
+	                <li data-target="#carouselExampleIndicators" data-slide-to="0" class=""></li>
+	                <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+	                <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
+	            </ol>
+	            <div class="carousel-inner" role="listbox">
+	                <div class="carousel-item">
+	                    <img class="d-block" src="/hospital.jpg" alt="First slide">
+	                    <div class="carousel-caption d-none d-md-block">
+	                        <h5>Nature, United States</h5>
+	                    </div>
+	                </div>
+	                <div class="carousel-item active">
+	                    <img class="d-block" src="/hospital.jpg" alt="Second slide">
+	                    <div class="carousel-caption d-none d-md-block">
+	                        <h5>Somewhere Beyond, United States</h5>
+	                    </div>
+	                </div>
+	                <div class="carousel-item">
+	                    <img class="d-block" src="/hospital.jpg" alt="Third slide">
+	                    <div class="carousel-caption d-none d-md-block">
+	                        <h5>Yellowstone National Park, United States</h5>
+	                    </div>
+	                </div>
+	            </div>
+	            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	                <i class="now-ui-icons arrows-1_minimal-left"></i>
+	            </a>
+	            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	                <i class="now-ui-icons arrows-1_minimal-right"></i>
+	            </a>
+	        </div>
+	</div>
+<!-- / -->
+
+
+
+
+
 		    </div>
 
 
