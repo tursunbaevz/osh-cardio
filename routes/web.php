4ca0098483390540698d4786d8304cb3dff0e218
Kozhonazar <rysbaev.k@gmail.com>
2018-03-22 17:40:43 +0600

    Some changes

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,7 @@ Auth::routes();
 
 Route::group(['middleware' => 'can:accessAdminpanel'], function() {
     Route::get('/adminpanel/dashboard', 'Adminpanel\Dashboard@index');
+    Route::get('/adminpanel/dashboard/posts', 'Adminpanel\Posts@index');
     // future adminpanel routes also should belong to the group
 });
+
