9bc2cf5711520e67a88756972a033a0e962602ef
Kozhonazar <rysbaev.k@gmail.com>
2018-03-22 17:17:22 +0600

    Created tables for Posts

--- database/migrations/2018_03_22_111206_create_posts_table.php
+++ database/migrations/2018_03_22_111206_create_posts_table.php
@@ -15,7 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('body');
             $table->timestamps();
+
         });
     }
 
