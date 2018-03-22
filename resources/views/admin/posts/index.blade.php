3350cbb93ee42c96819fb463998f621159274e72
Kozhonazar <rysbaev.k@gmail.com>
2018-03-22 18:32:48 +0600

    Last changes

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,25 +6,18 @@
 
 @section('content')
 
-	
 <div class="container">
-	<div class="row">
-		<div class="col">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore recusandae placeat dolores, officia ipsum deleniti tenetur voluptatem earum tempore reiciendis dignissimos, fugiat id adipisci natus consequatur voluptatibus ex libero.
-		</div>
-		<div class="col">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, illo, eos. Omnis, explicabo? Repellat minima in laboriosam illo voluptates nostrum minus esse nisi, dolor quos earum, illum perferendis. Natus, ipsa?
-		</div>
+<div class="row">
+	<div class="col-4"></div>
+	<div class="col-8">
+		@foreach($posts as $post)
+			<div class="row">
+				<h1>{{ $post->title }}</h1><br>
+				<h6>{{ $post->body }}</h6>
+			</div>
+		@endforeach
 	</div>
 </div>
-{{--
-	@foreach($posts as $post)
-
-		{{ $post->title }}
-		{{ $post->body }}
-
-	@endforeach
-
+</div>
 
 @endsection
---}}
