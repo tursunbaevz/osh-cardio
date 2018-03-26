1dca786c06b9989876ab233dbf549c3d6856cf19
tursunbaevz <zuma9405@gmail.com>
2018-03-26 16:59:12 +0600

    change background image for form section

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -105,7 +105,7 @@
 	                <div class="card card-pricing card-background card-raised" style="background-image: url('/img/block3-stac.jpg')">
 	                    <div class="card-body">
 	                    	<div class="more-services">
-		                        <h3 class="category text-info center-block-btn">Стационарные <br> лечения</h3>
+		                        <h3 class="category text-info center-block-btn">Стационарное <br> лечение</h3>
 		                        <a href="#pablo" class="btn icon-small btn-round m-style">
 		                            подробнее
 		                        </a>
@@ -130,49 +130,6 @@
 	    </div>
 	</div>
 	<!-- / -->
-
-
-	<!-- О нас -->
-	<!-- <div class="section section-about-us">
-	    <div class="container">
-	        <div class="row">
-	            <div class="col-md-8 ml-auto mr-auto text-center">
-	                <h2 class="title">Who we are?</h2>
-	                <h5 class="description">According to the National Oceanic and Atmospheric Administration, Ted, Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs year down to low ice extent in the Pacific and a late drop in ice extent in the Barents Sea.</h5>
-	            </div>
-	        </div>
-	        <div class="separator separator-primary"></div>
-	        <div class="section-story-overview">
-	            <div class="row">
-	                <div class="col-md-6"> -->
-	                    <!-- First image on the left side -->
-	                    <!-- <div class="image-container image-left" style="background-image: url('../assets/img/bg38.jpg')">
-	                        <p class="blockquote blockquote-primary">"Over the span of the satellite record, Arctic sea ice has been declining significantly, while sea ice in the Antarctichas increased very slightly"
-	                            <br>
-	                            <br>
-	                            <small>-NOAA</small>
-	                        </p>
-	                    </div> -->
-	                    <!-- Second image on the left side of the article -->
-	                  <!--   <div class="image-container image-left-bottom" style="background-image: url('../assets/img/bg24.jpg')"></div>
-	                </div>
-	                <div class="col-md-5"> -->
-	                    <!-- First image on the right side, above the article -->
-	                    <!-- <div class="image-container image-right" style="background-image: url('../assets/img/bg39.jpg')"></div>
-	                    <h3>So what does the new record for the lowest level of winter ice actually mean</h3>
-	                    <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that process will continue whatever happens with climate change. Even if the Arctic continues to be one of the fastest-warming regions of the world, it will always be plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of natural reasons, there’s huge variety of the state of the ice.
-	                    </p>
-	                    <p>
-	                        For a start, it does not automatically follow that a record amount of ice will melt this summer. More important for determining the size of the annual thaw is the state of the weather as the midnight sun approaches and temperatures rise. But over the more than 30 years of satellite records, scientists have observed a clear pattern of decline, decade-by-decade.
-	                    </p>
-	                    <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that process will continue whatever happens with climate change. Even if the Arctic continues to be one of the fastest-warming regions of the world, it will always be plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of natural reasons, there’s huge variety of the state of the ice.
-	                    </p>
-	                </div>
-	            </div>
-	        </div>
-	    </div>
-	</div> -->
-	<!-- /О Нас -->
 
 	<!-- блок краткая инфа -->
 	<div class="testimonials-1 section-image parallax">
@@ -262,7 +219,7 @@
 
 	<!-- обратная связь -->
 
-	<div class="section background-parallax" style="background: url('/img/pic-1.jpg');">
+	<div class="section background-parallax" style="background: url('/img/pic-pic2.jpg');">
 		<div class="pricing-2">
 		    <div class="container">
 		        <div class="row">
