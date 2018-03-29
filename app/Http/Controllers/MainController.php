3f8ac096a2d76be151e40a120a7029c3bd611dc7
Kozhonazar <rysbaev.k@gmail.com>
2018-03-29 23:55:02 +0600

    Created Posts list & Post  pages

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,19 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Main;
+use App\Post;
 
 class MainController extends Controller
 {
 
-	public function index()
 
-	{
+	public function index(){
 		return view('main.index');
 	}
 
-	public function layout(){
-		return view('layouts.layout');
+	public function postsList(){
+		$posts = Post::all();
+		return view('main.postsList', compact('posts'));
 	}
+
+	public function eachPost($id){
+		$eachPost = Post::find($id);
+		return view('main.eachPost', compact('eachPost'));
+	}
+
 
 
 }
---
+++ resources/views/main/postsList.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layoutForPosts')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-9" style="margin:auto; margin-top: 100px;">
+			@foreach($posts as $post)
+			<a href="/posts/{{$post->id}}" style="color: #000">
+				<h3>{{$post->title}}</h3>
+			</a>
+			<p>{{str_limit($post->body, 180)}}</p>	
+
+			@endforeach
+			</div>
+		</div>
+	</div>
+@endsection
