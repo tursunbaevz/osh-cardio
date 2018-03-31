814485c31593b604b9807ad917694c6f5af4bccd
tursunbaevz <zuma9405@gmail.com>
2018-03-31 16:27:52 +0600

    working on info block

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,9 +9,11 @@
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 
+
 	<!--     Fonts and icons     -->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
 
 	<!-- CSS Files -->
 	<link href="/css/bootstrap.min.css" rel="stylesheet" />
